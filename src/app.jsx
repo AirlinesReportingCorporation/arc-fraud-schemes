86260f9ca03c58b2be12d7414f6d05faea3f60f0
Bree Armstrong <barmstrong@arccorp.com>
2024-01-30 11:17:24 -0500

    updates to Fraud page

--- src/app.jsx
+++ src/app.jsx
@@ -7,21 +7,31 @@ import { Stickynav } from "arccorp-vars";
 import alerts from "./alerts.js";
 import schemes from "./schemes.js";
 
+const highlightedIds = 1;
+
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      alertToggle: false,
+      schemeToggle: false,
+    };
   }
 
   render() {
-  let alertOrder = [6, 1, 2, 3, 4, 5]
-  let newAlerts = alertOrder.map(function(alertOrder) {
-    return(alerts[alertOrder - 1]);
-  })
-  // console.log(newAlerts);
+    // Set the order of the Array
+    let alertOrder = [6, 1, 2, 3, 4, 5];
+    let newAlerts = alertOrder.map(function (alertOrder) {
+      return alerts[alertOrder - 1];
+    });
+    // set a local variable equal to the state
+    let alertShow = this.state.alertToggle;
+    let schemeShow = this.state.schemeToggle;
+
     return (
       <div className="arc-fraud-schemes">
         <Stickynav
-        style={{zIndex: "8"}}
+          style={{ zIndex: "8" }}
           title="Fraud Schemes"
           stickyCTA="Report Fraud"
           stickyCTALink="https://www2.arccorp.com/support-training/fraud-prevention/report-fraud/"
@@ -55,22 +65,29 @@ class App extends Component {
           </div>
           <br />
           <br />
-          <h2 className="mainTitle">
-            <a id="alerts"></a>Alerts
-          </h2>
-          <p>
-            ARC posts the latest information on fraud schemes, scams and
-            criminals within the travel industry and tips on how to protect your
-            business from fraud.
-          </p>
-          <hr />
-          <div className="row">
-            <div className="col-lg-12">
-              {newAlerts.map(function (alert) {
+          <div className="fraud-alerts" id="alerts">
+            <h2 className="mainTitle">Alerts</h2>
+            <p>
+              ARC posts the latest information on fraud schemes, scams and
+              criminals within the travel industry and tips on how to protect
+              your business from fraud.
+            </p>
+            <hr />
+            <div
+              className="show-button"
+              onClick={() =>
+                this.setState({ alertToggle: !this.state.alertToggle })
+              }
+            >
+              { this.state.alertToggle ? <span className="alerts-open">Collapse All Alerts</span> : <span className="alerts-close">Expand All Alerts</span> }
+            </div>
+            <div className="row">
+              {newAlerts.map(function (alert, i) {
                 return (
                   <Accordion
+                    highlighted={i < highlightedIds ? true : false}
+                    showing={alertShow}
                     key={alert.id}
-                    id={alert.id}
                     name={alert.name}
                     body={alert.body}
                     anchor={alert.anchor}
@@ -80,33 +97,41 @@ class App extends Component {
               })}
             </div>
           </div>
-          <hr />
-          <h2 className="mainTitle">
-            <a id="schemes"></a>Schemes
-          </h2>
-          <div className="scheme-info">
-            <p>
-              Fraudsters are often creative in the schemes they devise to
-              manipulate the customer service skills of travel agents who always
-              want to help their clients. The information they provide makes
-              them appear as legitimate customers with plausible reasons for
-              ordering tickets for themselves and others with close-in departure
-              dates. The fraudsters create a compelling story as to why an agent
-              should help them or create a sense of urgency to get that agent to
-              lower their guard and get the tickets issued. Once those tickets
-              are issued, the fraudster alerts his own customers that they are
-              ready to travel.
-            </p>
-            <p>
-              Below you’ll find schemes that are commonly employed by
-              fraudsters.
-            </p>
-          </div>
-          <div className="row">
-            <div className="col-lg-12">
-              {schemes.map(function(scheme) {
+          <div className="fraud-schemes" id="schemes">
+            <h2 className="mainTitle">Schemes</h2>
+            <div className="scheme-info">
+              <p>
+                Fraudsters are often creative in the schemes they devise to
+                manipulate the customer service skills of travel agents who
+                always want to help their clients. The information they provide
+                makes them appear as legitimate customers with plausible reasons
+                for ordering tickets for themselves and others with close-in
+                departure dates. The fraudsters create a compelling story as to
+                why an agent should help them or create a sense of urgency to
+                get that agent to lower their guard and get the tickets issued.
+                Once those tickets are issued, the fraudster alerts his own
+                customers that they are ready to travel.
+              </p>
+              <p>
+                Below you’ll find schemes that are commonly employed by
+                fraudsters.
+              </p>
+              <hr />
+              <div
+              className="show-button"
+              onClick={() =>
+                this.setState({ schemeToggle: !this.state.schemeToggle })
+              }
+            >
+              { this.state.schemeToggle ? <span className="alerts-open">Collapse All Schemes</span> : <span className="alerts-close">Expand All Schemes</span> }
+            </div>
+            </div>
+            <div className="row">
+              {schemes.map(function (scheme) {
                 return (
                   <Accordion
+                    showing={schemeShow}
+                    className="fraud-box"
                     key={scheme.id}
                     id={scheme.id}
                     name={scheme.name}
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,59 +1,83 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Accordion(props) {
   const [isShowing, setShowing] = useState(false);
 
- const toggleShow = () => {
-    setShowing(!isShowing);
-    console.log(isShowing)
-  };
+  useEffect(() => {
+    let showing = props.showing;
+    if (showing) {
+      setShowing(true);
+    }
+    else {
+      setShowing (false)
+    }
+  }, [props.showing])
 
+
+
+
+  const toggleShow = () => {
+    setShowing(!isShowing);
+    console.log(isShowing);
+  };
+  
   return (
     <div className="col-lg-12 show">
-      <div>
-        <div>
-          <div className={isShowing ? "airlinePartRow active" : "airlinePartRow"} onClick={toggleShow}>
-            <div className="airlinePartRowTop">
-              <div className="airlinePartRowStart container-fluid">
-                <div className="row align-items-center">
-                  <div className="col-11">
-                    <div className="d-flex flex-column flex-lg-row">
-                      <div className="">
-                        <div className="airlinePartName" id={props.anchor}>
-                          {props.name}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-1">
-                    <div>
-                      <div className="apExpand">
-                        <div className={isShowing ? "fs-close" : "fs-open"}></div>
-                      </div>
+      <div className={(isShowing ? "airlinePartRow active alert-box" : "airlinePartRow alert-box") + (props.highlighted ? " fraud-highlight" : "")}>
+        <div className="airlinePartRowTop">
+           <div
+            className={
+              isShowing
+                ? "airlinePartRowStart container-fluid active"
+                : "airlinePartRowStart container-fluid"
+            }
+            onClick={toggleShow}
+          >
+            <div className="row align-items-center">
+              <div className="col-11">
+                <div className="d-flex flex-column flex-lg-row">
+                  <div className="">
+                    <div className="airlinePartName" id={props.anchor}>
+                      {props.name}
                     </div>
                   </div>
                 </div>
               </div>
-              <div className={isShowing? "airlinePartRowInfo show animated slideInDown" : "airlinePartRowInfo"}>
-                  <div className="row no-gutters">
-                    <div className="col">
-                      <div className="apSection apSectionMargin">
-                       {props.updated != "" ?  <div className="apSectionTop">
-                          <div className="d-flex align-items-center">
-                            <div className="mr-1">
-                              <div className="apUpdated">
-                                <span>Updated:</span> {props.updated}
-                              </div>
-                            </div>
+              <div className="col-1">
+                <div>
+                  <div className="apExpand">
+                    <div className={isShowing ? "fs-close" : "fs-open" }></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className={
+              isShowing
+                ? "airlinePartRowInfo show animated slideInDown"
+                : "airlinePartRowInfo"
+            }
+          >
+            <div className="row no-gutters">
+              <div className="col">
+                <div className="apSection apSectionMargin">
+                  {props.updated != "" ? (
+                    <div className="apSectionTop">
+                      <div className="d-flex align-items-center">
+                        <div className="mr-1">
+                          <div className="apUpdated">
+                            <span>Updated:</span> {props.updated}
                           </div>
-                        </div> : ""}
-                        <div className="apSectionBottom">
-                          {props.body}
                         </div>
                       </div>
                     </div>
-                  </div>
+                  ) : (
+                    ""
+                  )}
+                  <div className="apSectionBottom">{props.body}</div>
                 </div>
+              </div>
             </div>
           </div>
         </div>
