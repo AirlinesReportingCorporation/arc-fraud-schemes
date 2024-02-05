67e544ffe74a46593eb04dca746da84cd0d674bf
Bree Armstrong <barmstrong@arccorp.com>
2024-02-05 14:42:23 -0500

    prep for new headers

--- src/app.jsx
+++ src/app.jsx
@@ -52,8 +52,8 @@ class App extends Component {
             </a>
             <h1>Fraud Alerts & Schemes</h1>
             <p className="byline">
-              Fraudsters are often creative in the schemes they devise to
-              manipulate the customer service skills of travel agents.
+              Defend against fraud with ARC's alerts and gain insights into
+              fraudsters' scheme tactics.
             </p>
             <a
               href="https://www2.arccorp.com/support-training/fraud-prevention/report-fraud/"
@@ -63,23 +63,30 @@ class App extends Component {
               Report Fraud
             </a>
           </div>
-          <br />
-          <br />
           <div className="fraud-alerts" id="alerts">
-            <h2 className="mainTitle">Alerts</h2>
-            <p>
-              ARC posts the latest information on fraud schemes, scams and
-              criminals within the travel industry and tips on how to protect
-              your business from fraud.
-            </p>
-            <hr />
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="alert-header">
+                  <h2 className="mainTitle">Alerts</h2>
+                  <p>
+                    Stay informed on the latest travel industry fraud with ARC's
+                    updates, providing essential tips to safeguard your
+                    business.
+                  </p>
+                </div>
+              </div>
+            </div>
             <div
               className="show-button"
               onClick={() =>
                 this.setState({ alertToggle: !this.state.alertToggle })
               }
             >
-              { this.state.alertToggle ? <span className="alerts-open">Collapse All Alerts</span> : <span className="alerts-close">Expand All Alerts</span> }
+              {this.state.alertToggle ? (
+                <span className="alerts-open">Collapse All Alerts</span>
+              ) : (
+                <span className="alerts-close">Expand All Alerts</span>
+              )}
             </div>
             <div className="row">
               {newAlerts.map(function (alert, i) {
@@ -98,33 +105,30 @@ class App extends Component {
             </div>
           </div>
           <div className="fraud-schemes" id="schemes">
-            <h2 className="mainTitle">Schemes</h2>
-            <div className="scheme-info">
-              <p>
-                Fraudsters are often creative in the schemes they devise to
-                manipulate the customer service skills of travel agents who
-                always want to help their clients. The information they provide
-                makes them appear as legitimate customers with plausible reasons
-                for ordering tickets for themselves and others with close-in
-                departure dates. The fraudsters create a compelling story as to
-                why an agent should help them or create a sense of urgency to
-                get that agent to lower their guard and get the tickets issued.
-                Once those tickets are issued, the fraudster alerts his own
-                customers that they are ready to travel.
-              </p>
-              <p>
-                Below you’ll find schemes that are commonly employed by
-                fraudsters.
-              </p>
-              <hr />
-              <div
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="scheme-header">
+                  <h2 className="mainTitle">Schemes</h2>
+                  <p>
+                    Learn how fraudsters deceive travel agents, gaining insights
+                    to protect your business. Below, you'll find a list of
+                    tactics frequently employed by fraudsters.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div
               className="show-button"
               onClick={() =>
                 this.setState({ schemeToggle: !this.state.schemeToggle })
               }
             >
-              { this.state.schemeToggle ? <span className="alerts-open">Collapse All Schemes</span> : <span className="alerts-close">Expand All Schemes</span> }
-            </div>
+              {this.state.schemeToggle ? (
+                <span className="alerts-open">Collapse All Schemes</span>
+              ) : (
+                <span className="alerts-close">Expand All Schemes</span>
+              )}
             </div>
             <div className="row">
               {schemes.map(function (scheme) {
